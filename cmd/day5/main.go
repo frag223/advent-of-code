e1079879145c989889e77bf587729cda196b60eb
mauricio riquelme <m.riquelme223@gmail.com>
2021-12-13 22:18:07 +1100

    wip

--- cmd/day5/main.go
+++ cmd/day5/main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//go:embed test.txt
+var input string
 
 func main() {
+
+	i := parseInput(input)
+
+	fmt.Println(i)
 
 	d := NewDiagram(10, true)
 	d.PlaceLine(Position{
@@ -10,5 +22,35 @@ func main() {
 		Y: 1,
 	}, Position{X: 1, Y: 9})
 	fmt.Println("--------")
-	fmt.Println(d.Grid)
+}
+
+func parseInput(input string) [][]Position {
+	list := strings.Split(input, "\n")
+	ff := [][]Position{}
+	for _, row := range list {
+		positionsRaw := strings.Split(row, "->")
+		firstPosition := strings.Split(
+			strings.TrimSpace(positionsRaw[0]),
+			",",
+		)
+		secondPosition := strings.Split(
+			strings.TrimSpace(positionsRaw[1]),
+			",",
+		)
+		x1, _ := strconv.Atoi(firstPosition[0])
+		y1, _ := strconv.Atoi(firstPosition[1])
+		x2, _ := strconv.Atoi(secondPosition[0])
+		y2, _ := strconv.Atoi(secondPosition[1])
+		ff = append(ff, []Position{
+			{
+				X: x1,
+				Y: y1,
+			},
+			{
+				X: x2,
+				Y: y2,
+			},
+		})
+	}
+	return ff
 }
