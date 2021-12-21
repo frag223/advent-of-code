dbddc12e4691b6c905520dd3d34e1281870a916a
mauricio riquelme <m.riquelme223@gmail.com>
2021-12-21 20:52:32 +1100

    could not solve this

--- cmd/day6/main.go
+++ cmd/day6/main.go
@@ -3,44 +3,81 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 //go:embed input.txt
 var input string
 
 func main() {
-	// fishData := parseText(input)
 	fishData := parseInput(input)
-	newData := fishData[0]
-	days := 256
-	for i := 0; i < days; i++ {
-		row := calculateNextRow(newData)
-		newData = row
-	}
-	fmt.Println(len(newData))
+
+	// part1 := predictPopulation(fishData, 80)
+	// fmt.Printf("part 1: %d\n", part1)
+
+	part2 := predictPopulation(fishData, 256)
+	fmt.Printf("part 2: %d\n", part2)
 }
 
-func calculateNextState(current int) (int, bool) {
-	switch current {
-	case 0:
-		return 6, true
-	default:
-		return current - 1, false
+// NOTE did not solve this
+
+// Use a circular buffer to keep track of the population of fishes.
+// At each generation, fishes who reach a 0 lifespan give birth to new fishes
+// (with life=8), and their life is reset to 6.
+//
+// Initial state:
+// life:  0   1   2   3   4   5   6   7   8
+// count: 0   1   1   2   1   0   0   0   0
+//
+// 1st generation
+// life:  8   0   1   2   3   4   5   6   7
+// count: 0   1   1   2   1   0   0   0   0
+//
+// 2nd generation
+// life:  7   8   0   1   2   3   4   5   6
+// count: 0  (1)  1   2   1   0   0   0   0+1
+//
+// 3rd generation
+// life:  6   7   8   0   1   2   3   4   5
+// count: 0+1 1  (1)  2   1   0   0   0   1
+//
+// 4th generation
+// life:  5   6   7   8   0   1   2   3   4
+// count: 1   1+2 1  (2)  1   0   0   0   1
+func predictPopulation(input []uint64, gens int) uint64 {
+	var buf [9]uint64
+	copy(buf[:], input)
+
+	born, reset := 8, 6
+	for i := 0; i < gens; i++ {
+		born, reset = incr(born), incr(reset)
+		buf[reset] += buf[born]
 	}
+
+	var result uint64
+	for _, count := range buf {
+		result += count
+	}
+	return result
 }
 
-func calculateNextRow(row []int) []int {
-	newFish := []int{}
-	nextRow := []int{}
-	for _, item := range row {
-		next, add := calculateNextState(item)
-		if add {
-			newFish = append(newFish, 8)
+func incr(index int) int {
+	if index == 8 {
+		return 0
+	}
+	return index + 1
+}
+
+func parseInput(line string) []uint64 {
+	inputStrings := strings.Split(line, ",")
+	var input [9]uint64
+	for _, s := range inputStrings {
+		n, err := strconv.Atoi(s)
+		if err != nil {
+			panic(err)
 		}
-		nextRow = append(nextRow, next)
+		input[n]++
 	}
-	if len(newFish) > 0 {
-		nextRow = append(nextRow, newFish...)
-	}
-	return nextRow
+	return input[:]
 }
